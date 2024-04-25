36b40c4ba1832b3a99b66aa05e39261c4cf58abb
Olena Nesteruk <pavlena81@gmail.com>
2024-04-25 16:58:28 +0300

    added modal to section destination

--- index.js
+++ index.js
@@ -26,3 +26,15 @@ console.log(myButton);
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// data modal 
+
+//  (() => {
+//     const refs = {
+//         openMenuBtn: document.querySelector(".data-modal3-open"),
+//         closeMenuBtn: document.querySelector(".data-modal3-close"), menu: document.querySelector(".data-modal3"),
+//         body: document.querySelector(".body"),
+//     }; refs.openMenuBtn.addEventListener("click", toggleMenu); refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//     function toggleMenu() { refs.menu.classList.toggle("is-hidden"); refs.body.classList.toggle("no-scroll"); }
+// })();
---
+++ modal.js
@@ -0,0 +1,12 @@
+(() => {
+    const refs = {
+        openMenuBtn: document.querySelector(".data-modal3-open"), 
+        closeMenuBtn: document.querySelector(".data-modal3-close"), menu: document.querySelector(".data-modal3"),
+        body: document.querySelector("body"),
+    }; refs.openMenuBtn.addEventListener("click", toggleMenu); refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    function toggleMenu() { refs.menu.classList.toggle("is-hidden"); refs.body.classList.toggle("no-scroll"); }
+
+console.log("Menu element:", refs.menu);
+})();
+
+
